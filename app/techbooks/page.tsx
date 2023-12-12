67a47e9c8313845b04d79d96316e26776efd605b
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-12 05:32:02 +0100

    without images

--- app/techbooks/page.tsx
+++ app/techbooks/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+import { AppContext } from "@/AppContext";
+import { useContext } from "react";
+
 export default function PageTechBooks() {
+	const { techBooks } = useContext(AppContext);
+
 	return (
 		<>
-			<p>This is the tech books page.</p>
+			<p className="mb-4">There are {techBooks.length} tech books:</p>
+			{techBooks.map((techBook) => {
+				return (
+					<div className="mb-3 flex gap-3 w-1/3" key={techBook.id}>
+						<p>IMAGE</p>
+						<div>
+							<p className="font-bold text-xl">{techBook.title}</p>
+							<p className="italic">{techBook.description}</p>
+						</div>
+					</div>
+				);
+			})}
 		</>
 	);
 }
