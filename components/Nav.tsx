50bbbc3d120a78deea5205635220c9e32ea0b87e
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-12 04:03:27 +0100

    first link colored

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,22 @@
+"use client";
 import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
 
 export const Nav = () => {
+	const activeSegment = useSelectedLayoutSegment();
+
 	return (
 		<nav className="bg-slate-600 text-white mb-4 rounded p-2 mainnav">
 			<ul className="flex gap-4 ml-1">
 				<li>
-					<Link href="/">Welcome</Link>
+					<Link
+						href="/"
+						className={`${
+							activeSegment === null ? "text-yellow-200" : ""
+						}`}
+					>
+						Welcome
+					</Link>
 				</li>
 				<li>
 					<Link href="/techskills">Tech Skills</Link>
